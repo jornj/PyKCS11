d97a53a5971c42cb99a11ceaeceb25cfdb451ce2
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-04-18 15:52:59 +0200

    test_symetric: test for AES_GCM_Mechanism

--- test/test_symetric.py
+++ test/test_symetric.py
@@ -93,4 +93,17 @@ class TestUtil(unittest.TestCase):
         # 2nd block
         self.assertSequenceEqual(DataOut[16:], DataECBOut2)
 
+        #
+        # test CK_GCM_PARAMS
+        #
+
+        AES_GCM_IV_SIZE = 12
+        AES_GCM_TAG_SIZE = 16
+        iv = [42] * AES_GCM_IV_SIZE
+        aad = "plaintext aad"
+        mechanism = PyKCS11.AES_GCM_Mechanism(iv, aad, AES_GCM_TAG_SIZE)
+
+        DataOut = self.session.encrypt(symKey, DataIn, mechanism)
+        print("DataOut", DataOut)
+
         self.session.destroyObject(AESKey)
